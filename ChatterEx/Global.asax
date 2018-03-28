10e5e9edc9933e3f109dd4201fd75d44016c4f7c
bethbenjamin <bethbenjamin@gmail.com>
2018-03-28 10:13:47 -0400

    first commit

---
+++ ChatterEx/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatterEx.MvcApplication" Language="C#" %>
---
+++ ChatterEx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterEx.Startup))]
+namespace ChatterEx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
